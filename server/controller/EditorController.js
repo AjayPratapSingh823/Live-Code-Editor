aa2e0cff01a7bbc5444a02ae8ae4ae2abf6f7269
AjayPratapSingh823 <80371996+AjayPratapSingh823@users.noreply.github.com>
2024-07-09 08:56:09 +0000

    compiler done

--- server/controller/EditorController.js
+++ server/controller/EditorController.js
@@ -6,28 +6,33 @@ const Compiler=async(req,res)=>{
     let language= req.body.language;
     let input= req.body.input;
     if(language==='python'){
-        language="py"
+        language="python3"
     }
     let data=({
-        "code":code,
-        "language":language,
-        "input":input
+        script: code,
+        language: language.toLowerCase(),
+        stdin: input
     })
 
     let config={
         method:"post",
         url:
-        'https://codexweb.netlify.app/.netlify/functions/enforceCode',
+        'https://api.jdoodle.com/v1/execute',
         headers: {
             'Content-Type': 'application/json'
         },
-        data: data
+        data: {
+            ...data,
+            clientId: '8d55963f5c9d7f62835013864d576da1',
+            clientSecret: '6f76871084592905b16ef81ab302dfa7427f72c7fb1432cb828f40a18c549cd2'
+        }
     };
     try{
     const response=await axios(config);
     return res.send(response.data);
     }catch(err){
         console.log(err);
+        return res.status(500).send('Error executing the code');
     }
 };
 module.exports={Compiler};
--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const axios = require('axios')
 const app = express()
 const PORT=8000;
 const EditorRoutes=require('./routes/EditorRoutes')
+
+
+
+
 app.use(cors());
 app.use(express.json());
 app.use('/api',EditorRoutes);
